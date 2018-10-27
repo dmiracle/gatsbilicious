0ea4e68e4e412ce102db76ba850076c7f89e1312
Anisa <anisa.ali7@gmail.com>
2018-10-27 14:04:52 -0500

    test push to master

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -30,7 +30,7 @@ class Bio extends React.Component {
           Written by <strong>Kyle Mathews</strong> who lives and works in San
           Francisco building useful things.{' '}
           <a href="https://twitter.com/kylemathews">
-            You should follow him on Twitter
+            You should follow him on Twitter. jk
           </a>
         </p>
       </div>

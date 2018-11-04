33f5356106e33ded06c5c94717112e0bd189fe7a
Dylan Miracle <dylan.miracle@gmail.com>
2018-11-04 16:26:48 -0600

    added header

---
+++ src/components/header/header.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import './header.sass'
+
+export default () => {
+  <div className='header-style'><h1>This is a Header (it's SASSy)</h1></div>
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
 import React from "react"
+import Footrino from '../components/footer/footer'
+import Headerino from '../components/header/header'
 
-export default () => <div>Hello world!</div>
+export default () => {
+  return(
+    <div>
+      <Headerino/>
+      here's some text woo, and here's a footer:
+      <Footrino/>
+    </div>
+  )
+}
